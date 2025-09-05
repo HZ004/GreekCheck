c1fd3bd66832200152d4c846b507d1e7c6ecd1e5
HZ004 <97055360+HZ004@users.noreply.github.com>
2025-09-05 15:19:10 +0530

    Delete frontend directory

--- frontend/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import PlotlyStreamer from "./PlotlyStreamer";
-
-const container = document.getElementById("root");
-
-const root = ReactDOM.createRoot(container!);
-root.render(<PlotlyStreamer dataJson={window.dataJson || ""} />);
