f80f1acb67519fc541cd4d02d91d6feae012dbb2
HZ004 <97055360+HZ004@users.noreply.github.com>
2025-09-10 20:52:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  preview: {
+    host: true,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    allowedHosts: ['greekcheck.onrender.com'] // Add your Render domain here
+  }
 })
